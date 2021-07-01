c4be9897e27e8c2e1ee5c7175a54fd6812836518
Michael Goin <mgoin@newrelic.com>
2021-07-01 12:31:00 -0700

    tracking root.

--- test/versioned/mongodb/cursor-duration.tap.js
+++ test/versioned/mongodb/cursor-duration.tap.js
@@ -49,67 +49,44 @@ tap.test('cursor duration tests', function(t) {
 
   t.test('toArray callback duration should be greater than its parent wrapper', function(t) {
     helper.runInTransaction(agent, function(transaction) {
+      console.log('root duration: ', transaction.trace.root.getDurationInMillis())
       collection.find({}).toArray(function onToArray(err, data) {
-        setImmediate(() => {
-          transaction.end()
-
-          const parent = transaction.trace.root
-          const [mongo] = parent.children
-          const [callback] = mongo.children
-
-          //const segment = agent.tracer.getSegment()
-          const cbTime = callback.getExclusiveDurationInMillis()
-          // current segment is this callback, must get its parent and parent's parent
-          const mongoTime = mongo.getExclusiveDurationInMillis()
-          const parentTime = parent.getExclusiveDurationInMillis()
-          console.log(`mongoName: ${mongo.name}, parentName: ${parent.name}, cbName: ${callback.name}`)
-          console.log('parentduration', parent.getDurationInMillis())
-          console.log('child count: ', parent.children.length)
-          console.log('mongoTime', mongoTime, 'parentTime', parentTime, 'cbTime', cbTime)
-          t.ok(mongoTime > parentTime, 'toArray duration should be longer than its parent')
-          t.notOk(err)
-          t.equal(data[0].i, 0)
-          t.end()
-        })
+        console.log('root duration: ', transaction.trace.root.getDurationInMillis())
+        const segment = agent.tracer.getSegment()
+        const cbTime = segment.getExclusiveDurationInMillis()
+        // current segment is this callback, must get its parent and parent's parent
+        const mongoTime = segment.parent.getExclusiveDurationInMillis()
+        const parentTime = segment.parent.parent.getExclusiveDurationInMillis()
+        console.log(`mongoName: ${segment.parent.name}, parentName: ${segment.parent.parent.name}, cbName: ${segment.name}`)
+        console.log('parentduration', segment.parent.parent.getDurationInMillis())
+        console.log('child count: ', segment.parent.parent.children.length)
+        console.log('mongoTime', mongoTime, 'parentTime', parentTime, 'cbTime', cbTime)
+        t.ok(mongoTime > parentTime, 'toArray duration should be longer than its parent')
+        t.notOk(err)
+        t.equal(data[0].i, 0)
+        t.end()
       })
     })
   })
 
   t.test('toArray promise duration should be greater than its parent wrapper', function(t) {
     helper.runInTransaction(agent, async function(transaction) {
+      console.log('root duration: ', transaction.trace.root.getDurationInMillis())
+
       const data = await collection.find({}).toArray()
-      // const segment = agent.tracer.getSegment()
-      // // asserts the toArray promise execution is longer than its parent
-      // // see https://github.com/newrelic/node-newrelic/issues/788
-      // const parentTime = segment.getExclusiveDurationInMillis()
-      // const parentDuration = segment.getDurationInMillis()
-      // const mongoTime = segment.children[0].getExclusiveDurationInMillis()
-      // console.log('mongoTime', mongoTime, 'parentTime', parentTime, 'parentDuration', parentDuration)
-      // console.log('child count: ', segment.children.length)
-      // t.ok(mongoTime > parentTime, 'toArray promise duration should be longer than its parent')
+      console.log('root duration: ', transaction.trace.root.getDurationInMillis())
+      const segment = agent.tracer.getSegment()
+      // asserts the toArray promise execution is longer than its parent
+      // see https://github.com/newrelic/node-newrelic/issues/788
+      const parentTime = segment.getExclusiveDurationInMillis()
+      const parentDuration = segment.getDurationInMillis()
+      const mongoTime = segment.children[0].getExclusiveDurationInMillis()
+      console.log('mongoTime', mongoTime, 'parentTime', parentTime, 'parentDuration', parentDuration)
+      console.log('child count: ', segment.children.length)
+      t.ok(mongoTime > parentTime, 'toArray promise duration should be longer than its parent')
 
-      // t.equal(data[0].i, 0)
-      // t.end()
-
-      setImmediate(() => {
-        transaction.end()
-
-        const parent = transaction.trace.root
-        const [mongo] = parent.children
-        // asserts the toArray promise execution is longer than its parent
-        // see https://github.com/newrelic/node-newrelic/issues/788
-        const parentTime = parent.getExclusiveDurationInMillis()
-        const parentDuration = parent.getDurationInMillis()
-        const mongoTime = mongo.getExclusiveDurationInMillis()
-
-        console.log(`mongoName: ${mongo.name}, parentName: ${parent.name}`)
-        console.log('mongoTime', mongoTime, 'parentTime', parentTime, 'parentDuration', parentDuration)
-        console.log('child count: ', parent.children.length)
-        t.ok(mongoTime > parentTime, 'toArray promise duration should be longer than its parent')
-
-        t.equal(data[0].i, 0)
-        t.end()
-      })
+      t.equal(data[0].i, 0)
+      t.end()
     })
   })
 })
